4a2d38846483a00c1ac9fe707401d6a4507332cc
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2024-05-25 10:28:36 +0300

    Add project files.

---
+++ Mikhalevich20331.UI/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mikhalevich20331.UI.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
