7587646b7f6e2491b31579020a642020b9d5690c
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2024-06-22 12:25:00 +0300

    Лаб 2

---
+++ Mikhalevich20331.UI/Views/Shared/Components/Cart/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mikhalevich20331.UI.Views.Shared.Components.Cart
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
